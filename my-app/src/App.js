7bd250790ebe7086cc8925d924c73323d0454d28
theguestmanuel <emmanuel.guimaraes@ufpi.edu.br>
2025-11-05 23:21:40 -0300

    fix dev flow 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Atividade de Engenharia de Software
+          Atividade de Engenharia de Software - Dev Version
         </p>
         <a
           className="App-link"
